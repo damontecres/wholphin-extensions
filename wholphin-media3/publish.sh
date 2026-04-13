4d08303684f3a397d024e3ff8053d85c8aaa0e78
Ray <154766448+damontecres@users.noreply.github.com>
2026-04-13 12:45:16 -0400

    Create a pom file for media3 extensions (#2)
    This PR adds generating `pom.xml` for the media3 ffmpeg & av1 extensions
    that have a explicit dependency on `media3-exoplayer`.
    
    This means downstream projects (eg Wholphin) will resolve the
    dependencies correctly fixing problems like
    https://github.com/damontecres/Wholphin/issues/1239.
    
    This is kind of hacky because the POM is written manually so changes in
    the gradle build dependencies in media3 could break this. But it is a
    workable stop gap for now.

--- wholphin-media3/publish.sh
+++ wholphin-media3/publish.sh
@@ -7,23 +7,27 @@ PROJECT_ROOT="$(realpath "${SCRIPT_DIR}/../")"
 TARGET_PATH="$PROJECT_ROOT/wholphin-media3/libs"
 SETTINGS_FILE="${PROJECT_ROOT}/wholphin-media3/mvn_settings.xml"
 
+media3_version="$(grep "androidx-media3 = " "$PROJECT_ROOT/gradle/libs.versions.toml" | awk -F'"' '{print $2}')"
+
 version="$(git describe --tags --abbrev=0)"
 version=${version#v}
+
+version=$version artifactId=decoder-ffmpeg media3_version=$media3_version \
+  envsubst '${version} ${artifactId} ${media3_version}' < "${PROJECT_ROOT}/wholphin-media3/pom.template.xml" > "${PROJECT_ROOT}/wholphin-media3/ffmpeg.pom.xml"
+
+version=$version artifactId=decoder-av1 media3_version=$media3_version \
+  envsubst '${version} ${artifactId} ${media3_version}' < "${PROJECT_ROOT}/wholphin-media3/pom.template.xml" > "${PROJECT_ROOT}/wholphin-media3/av1.pom.xml"
 
 mvn deploy:deploy-file \
   -s "$SETTINGS_FILE" \
   -Durl="https://maven.pkg.github.com/damontecres/wholphin-extensions" \
   -Dfile="$TARGET_PATH/lib-decoder-ffmpeg-release.aar" \
-  -DgroupId=com.github.damontecres.wholphin.media3 \
-  -DartifactId=decoder-ffmpeg \
-  -Dversion="$version" \
+  -DpomFile="${PROJECT_ROOT}/wholphin-media3/ffmpeg.pom.xml" \
   -DrepositoryId=github
 
 mvn deploy:deploy-file \
   -s "$SETTINGS_FILE" \
   -Durl="https://maven.pkg.github.com/damontecres/wholphin-extensions" \
   -Dfile="$TARGET_PATH/lib-decoder-av1-release.aar" \
-  -DgroupId=com.github.damontecres.wholphin.media3 \
-  -DartifactId=decoder-av1 \
-  -Dversion="$version" \
+  -DpomFile="${PROJECT_ROOT}/wholphin-media3/av1.pom.xml" \
   -DrepositoryId=github
